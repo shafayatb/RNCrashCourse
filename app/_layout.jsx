b6b923d1b1bf1755cfc4c64392b19685c953cfcf
shafayatb <mamunshafayatbin@gmail.com>
2024-05-27 18:19:58 +0600

    Removed header from all screens

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -16,19 +16,37 @@ const RootLayout = () => {
         "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
         "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
         "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-      });  
-    
+    });
+
     useEffect(() => {
-        if(error) throw error;
+        if (error) throw error;
 
-        if(fontsLoaded) SplashScreen.hideAsync();
-    }, [fontsLoaded, error])  
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error])
 
-    if(!fontsLoaded && !error) return null;
+    if (!fontsLoaded && !error) return null;
 
     return (
         <Stack>
-            <Stack.Screen name="index" options={{headerShown: false}}/>
+            <Stack.Screen
+                name="index"
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="(auth)"
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="(tabs)"
+                options={{ headerShown: false }}
+            />
+
+            {/* <Stack.Screen
+                name="(/search/[query])"
+                options={{ headerShown: false }}
+            /> */}
         </Stack>
     )
 }
