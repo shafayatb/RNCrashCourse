a0e2c936485cf09016b2f430f920c1973f0001bb
shafayatb <mamunshafayatbin@gmail.com>
2024-06-03 16:27:12 +0600

    Changed to ImagePicker in Create screen

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -6,6 +6,7 @@ import { Video, ResizeMode } from 'expo-av'
 import { icons } from '../../constants'
 import CustomButton from '../../components/CustomButton'
 import * as DocumentPicker from 'expo-document-picker';
+import * as ImagePicker from 'expo-image-picker';
 import { router } from 'expo-router'
 import { createVideo } from '../../lib/appWriteVideos'
 import { useGlobalContext } from '../../context/GlobalProvider'
@@ -22,9 +23,11 @@ const Create = () => {
   })
 
   const openPicker = async (selectType) => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type: selectType === 'image' ? ['image/*'] : ['video/mp4', 'video/gif']
-    })
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
     if (!result.canceled) {
       if (selectType === 'image') {
