94eb4947c95a6c92e85974a941fdb95eacd47b25
shafayatb <mamunshafayatbin@gmail.com>
2024-06-03 22:37:29 +0600

    Added ActivityIndicator in Custom Button

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React from 'react'
 
 const CustomButton = ({ title, handlePress, containeStyles, textStyles, isLoading }) => {
@@ -6,12 +6,21 @@ const CustomButton = ({ title, handlePress, containeStyles, textStyles, isLoadin
         <TouchableOpacity
             onPress={handlePress}
             activeOpacity={0.7}
-            className={`bg-secondary rounded-xl min-h-[48px] justify-center items-center ${containeStyles} ${isLoading ? 'opacity-50' : ''}`}
+            className={`bg-secondary rounded-xl min-h-[48px] flex flex-row justify-center items-center ${containeStyles} ${isLoading ? 'opacity-50' : ''}`}
             disabled={isLoading}
         >
             <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
                 {title}
             </Text>
+
+            {isLoading && (
+                <ActivityIndicator
+                    animating={isLoading}
+                    color="#fff"
+                    size="small"
+                    className="ml-2"
+                />
+            )}
         </TouchableOpacity>
     )
 }
