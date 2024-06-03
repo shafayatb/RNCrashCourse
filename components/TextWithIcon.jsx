acce21ed61249cdf495cef623929f8acbd876ba0
shafayatb <mamunshafayatbin@gmail.com>
2024-06-03 22:43:36 +0600

    Fixed margins in TextWithIcon component

--- components/TextWithIcon.jsx
+++ components/TextWithIcon.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const TextWithIcon = ({ icon, title }) => {
     return (
-        <View className="flex-row items-center justify-center pl-2">
+        <View className="flex-row items-center justify-center p-2 ml-3">
             <Image
                 source={icon}
                 className="w-5 h-5"
                 resizeMode='contain'
             />
-            <Text className={`text-base text-gray-100 font-pregular p-1 flex-1 ml-1 text-left`}>{title}</Text>
+            <Text className={`text-base text-gray-100 font-pregular flex-1 ml-2`}>{title}</Text>
         </View>
     )
 }
