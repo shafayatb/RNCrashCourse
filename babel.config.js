24fe8570cc51ab331cda9f33a9be33afbcc52db6
shafayatb <mamunshafayatbin@gmail.com>
2024-06-11 12:19:05 +0600

    babel file reformatted

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel",
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: 'config.env',
-      },
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: "config.env",
+        },
+      ],
     ],
-  ],
   };
 };
